7dda5c0711ca2275095dd8ec5f96a9ce136108e8
Carlton Shepherd <carlton@linux.com>
2018-09-19 21:00:53 +0200

    Docs

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="paillier-lib",
+    version="0.0.1",
+    author="Carlton Shepherd",
+    author_email="carlton@linux.com",
+    description="A simple implementation of the Paillier cryptosystem",
+    long_description=long_description,
+    url="https://github.com/carltonshepherd/paillier-lib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Security :: Cryptography",
+        "Intended Audience :: Science/Research",
+        "Development Status :: 3 - Alpha"
+    ],
+    install_requires=[
+        "gmpy2>=2.0.0",
+    ]
+)
